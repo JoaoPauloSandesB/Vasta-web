ced8875191d40962578ad8aa1b2ebc2027bc85f5
JoaoPauloSandesB <39538571+JoaoPauloSandesB@users.noreply.github.com>
2023-01-25 00:18:03 -0300

    Vasta Web site
    Arquivos necessários para o desenvolvimento e implantação do site.

---
+++ src/App.js
@@ -0,0 +1,217 @@
+import Header from './components/header/MainHeader';
+import { useState, useEffect } from 'react';
+
+
+import IMAGEM1 from './img/IMAGEM 1.jpg';
+import IMAGEM2 from './img/IMAGEM 2.jpg';
+import logoRodape from './img/footer_logo_360x.png';
+import img_portfolio_1 from './img/portfolio-imagem-1.jpg';
+import img_portfolio_2 from './img/portfolio-imagem-2.jpg';
+import img_portfolio_3 from './img/portfolio-imagem-3.jpg';
+import img_portfolio_4 from './img/portfolio-imagem-4.jpg';
+import img_portfolio_5 from './img/portfolio-imagem-5.jpg';
+import img_portfolio_6 from './img/portfolio-imagem-6.jpg';
+
+import config from './config.json';
+
+import Avatar from 'react-avatar';
+
+import './App.css';
+import './AppMedia.css';
+import './carossel.css';
+
+function App() {
+  const [carosselNumber, setCarosselNumber] = useState(0);
+  const [commentNumber, setCommentNumber] = useState(3);
+  const [selectSobre, setSelectSobre] = useState(false);
+  const [selectPortfolio, setSelectPortfolio] = useState(false);
+
+
+  useEffect(() => {
+    if (window.screen.width < 600) {
+      setCommentNumber(1);
+    } else {
+      setCommentNumber(3);
+    }
+    console.log("Tamanho da tela att");
+  }, []);
+
+  const NextCarossel = () => {
+    if (carosselNumber > (Object.keys(config['Comments']).length - commentNumber - 1))
+      setCarosselNumber(0);
+    else
+      setCarosselNumber(carosselNumber + 1);
+  };
+
+  const BeforeCarossel = () => {
+    if (carosselNumber <= 0)
+      setCarosselNumber((Object.keys(config['Comments']).length - commentNumber - 1));
+    else
+      setCarosselNumber(carosselNumber - 1);
+  };
+
+
+  return (
+    <div className="App">
+
+      <Header />
+
+
+      <div className="App-sobre">
+        <p className="titulo" id="sobre">SOBRE</p>
+        <div className="paragrafo-1">
+          <img src={IMAGEM1} className="image-1" alt="img" />
+          <div className="info">
+            <p>A Vasta Web é uma especializada em desenvolvimento de sites personalizados e profissionais
+              para empresas e indivíduos. Utilizamos as últimas tecnologias e tendências para criar sites
+              atraentes, funcionais e otimizados para mecanismos de busca. Oferecemos uma ampla gama de
+              serviços, desde a criação de sites desde o zero até a manutenção e otimização de sites
+              existentes. Nossa equipe de designers e desenvolvedores experientes trabalha em estreita
+              colaboração com nossos clientes para entender suas necessidades e criar soluções
+              personalizadas para atender aos seus objetivos de negócios. </p>
+            {!selectSobre && <button onClick={() => { setSelectSobre(!selectSobre) }} className="botao">SAIBA MAIS</button>}
+            {!selectSobre && <button onClick={() => { setSelectSobre(!selectSobre) }} className="botaoMobile">
+              <span className="material-icons">expand_more</span>
+              LEIA MAIS
+              <span className="material-icons">expand_more</span>
+            </button>}</div>
+        </div>
+
+        {selectSobre && <div className="paragrafo-2">
+          <img src={IMAGEM2} className="image-2-M" alt="img" />
+          <div className="info">
+            <p>Além de desenvolver sites, também oferecemos serviços de SEO e marketing digital para ajudar
+              a maximizar o alcance e a eficácia do seu site. Isso inclui a otimização de mecanismos de
+              busca, criação de conteúdo de qualidade, geração de leads e análise de dados para entender
+              como os visitantes estão interagindo com o seu site e como podemos melhorar a sua experiência.
+              Nós também oferecemos suporte contínuo e manutenção do site para garantir que ele continue
+              funcionando corretamente e esteja sempre atualizado com as últimas tendências e tecnologias.
+              Se você está procurando uma empresa confiável para ajudá-lo a criar ou melhorar o seu site,
+              a empresa Vasta Web é a escolha certa.</p>
+          </div>
+          <img src={IMAGEM2} className="image-2" alt="img" />
+        </div>}
+      </div>
+
+
+      <div className="App-portfolio" id="portfolio">
+        <p className="titulo">PORTFÓLIO</p>
+        <div className="linha-portfolio">
+          <img src={img_portfolio_1} className="image" alt="img" width="294" height="195" />
+          <img src={img_portfolio_2} className="image" alt="img" width="294" height="195" />
+          <img src={img_portfolio_3} className="image" alt="img" width="294" height="195" />
+          <img src={img_portfolio_4} className="image" alt="img" width="294" height="195" />
+          <img src={img_portfolio_5} className={selectPortfolio ? "image" : "image-M"} alt="img" width="294" height="195" />
+          <img src={img_portfolio_6} className={selectPortfolio ? "image" : "image-M"} alt="img" width="294" height="195" />
+        </div>
+        <button onClick={() => { setSelectPortfolio(!selectPortfolio) }} className="botao">CARREGAR MAIS PROJETOS</button>
+      </div>
+
+
+      <p className="titulo-depoimento" id="depoimentos">DEPOIMENTOS</p>
+      <div className="App-depoimentos">
+        <button className="icon_next" onClick={() => { BeforeCarossel(); }}>
+          <span className="material-icons">navigate_before</span>
+        </button>
+        {Object.keys(config['Comments']).map((comm, n) =>
+          <div key={n} className={(n >= carosselNumber && n < carosselNumber + commentNumber) ? 'box-depoimento-visible' : 'box-depoimento-invisible'}>
+
+            <div className="box-info">
+              <Avatar size="80" name="Joao Paulo" />
+              <div className="info">
+                <p className="nome">{comm}</p>
+                <p className="star">&#9733; &#9733; &#9733; &#9733; &#9733; </p>
+                <p className="dp"> {config['Comments'][comm][1]} </p>
+              </div>
+            </div>
+          </div>
+        )}
+        <button className="icon_next" onClick={() => { NextCarossel(); }}>
+          <span className="material-icons">navigate_next</span>
+        </button>
+      </div>
+
+
+      <p className="titulo-contato" id="contato">CONTATO</p>
+      <div className="App-contato">
+        <div className='personalinfo'>
+          <div><input type="nome" /* onChange={changeInfo} */ id="InputNome" placeholder="Nome"></input></div>
+          <div><input type="sobrenome" /* onChange={changeInfo} */ id="InputSobrenome" placeholder="Sobrenome"></input></div>
+          <div><input type="email" /* onChange={changeInfo} */ id="InputEmail" placeholder="Email"></input></div>
+        </div>
+        <div className='divdata'>
+          <div className='msg'>
+            <textarea type="mensagem" /* onChange={changeInfo} */ id="InputMensagem" placeholder="Mensagem"></textarea>
+          </div>
+          <div className='tellbutton'>
+            <input type="telefone" /* onChange={changeInfo} */ id="InputTelefone" placeholder="Telefone"></input>
+            <div className='msgbutton'>
+              <p>Todos os dados serão guardados de forma segura, saiba mais <a>aqui</a>.</p>
+              <button className="botao">ENVIAR</button></div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className='rodape'>
+        <div className='E'>
+          <div className="div-image-E">
+            <img src={logoRodape} className="image" alt="img" /></div>
+          <div>
+            <p className='news'> SE INSCREVA PARA RECEBER NOSSAS NOVIDADES</p>
+            <div>
+              <input type="emailnews" /* onChange={changeInfo} */ id="InputEmailNews" placeholder="Email"></input>
+              <button className="botao">INSCREVER</button>
+            </div>
+          </div>
+          <p className='tittle-port'>PORTFÓLIO</p>
+          <div className='port'>
+            <div className='port-div1'>
+              <div>
+                <img src={img_portfolio_2} className="image" alt="img" />
+                <p>LINK 1</p>
+              </div>
+              <div>
+                <img src={img_portfolio_3} className="image" alt="img" />
+                <p>LINK 2</p>
+              </div>
+            </div>
+            <div className='port-div1'>
+              <div>
+                <img src={img_portfolio_5} className="image" alt="img" />
+                <p>LINK 3</p>
+              </div>
+              <div>
+                <img src={img_portfolio_4} className="image" alt="img" />
+                <p>LINK 4</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='D'>
+          <div className='D-menu'>
+            <div className="d1">
+              <p className="p1"><span className="material-icons">expand_more</span> MENU <span className="material-icons">expand_more</span></p>
+              <div>
+                <p className="p2"><a href="#sobre">Sobre</a></p>
+                <p className="p2"><a href="#portfolio">Portfolio</a></p>
+                <p className="p2"><a href="#depoimentos">Depoimentos</a></p>
+                <p className="p2"><a href="#contato">Contato</a></p></div>
+            </div>
+            <div className="d2">
+              <p className="p2"><span className="material-icons">expand_more</span> SOBRE <span className="material-icons">expand_more</span></p>
+              <div>
+                <p className="p2">Politica de Privacidade</p>
+                <p className="p2">Termos de Uso</p></div>
+            </div>
+          </div>
+          <div className="div-image-D">
+            <img src={logoRodape} className="image" alt="img" /></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/MainHeader.js
@@ -0,0 +1,68 @@
+import { Routes, Route, BrowserRouter, Navigate, Switch, useNavigate } from "react-router-dom";
+
+import React, { useEffect, Fragment } from 'react';
+
+import './navbar.css';
+
+import logo from '../../img/Vasta.png';
+import banner from '../../img/Banner.jpg';
+import bannerMobile from '../../img/BANNER - IMAGE - MOBILE.png';
+
+const MainHeader = (props) => {
+  const navigate = useNavigate();
+
+  const update = () => {
+    props.onChangeState(true);
+  }
+
+  const toggleMenuOpen = () => { document.body.classList.toggle("open"); }
+  var title = "Vasta";
+
+  return (
+    <div className="headerstyle">
+
+      <img src={banner} className="App-banner-img" alt="Banner" />
+      <img src={bannerMobile} className="App-banner-img-media" alt="Banner" />
+      <div className="App-banner-tittle">
+          <p className="titulo">SOLUÇÕES EM DESENVOLVIMENTO WEB</p>
+          <p className="subtitulo">Projetos personalizados para o sucesso dos seus negócios!</p>
+          <button className="botao">FAÇA UM ORÇAMENTO</button>
+        </div>
+      <nav className="navbar">
+
+        <div className="navbar-overlay" onClick={toggleMenuOpen}></div>
+
+        <img src={logo} onClick={() => { navigate("/home"); }} className="navbar-title" alt="logo" />
+        
+
+
+        <nav className="navbar-menu">
+          <img src={logo} className="navbar-img-menu" alt="logo" />
+          <button><a href="#sobre">Sobre</a></button>
+          <button><a href="#portfolio">Portfólio</a></button>
+          <button><a href="#depoimentos">Depoimentos</a></button>
+          <button><a href="#contato">Contato</a></button>
+          <section>
+          <input type="search" id="InputSearch" placeholder="Search"></input>
+          <button onClick={() => { navigate("#search"); }}> <div> <span className="material-icons">search</span> </div></button>
+          </section>
+        </nav>
+
+
+
+        {/* <button onClick={() => { navigate("#search"); }} className="navbar-burger-search"> <span className="material-icons">search</span> </button>
+ */}
+        <button type="button" className="navbar-burger" onClick={toggleMenuOpen}>
+          <span className="material-icons">menu</span>
+        </button>
+
+      </nav>
+
+
+
+    </div>
+
+  );
+};
+
+export default MainHeader;
